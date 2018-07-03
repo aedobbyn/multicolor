1905211b41cd13d322356ee4156dca77ae2f62b8
aedobbyn <adobbs11@gmail.com>
2018-07-03 05:17:15 -0500

    export things

--- R/things.R
+++ R/things.R
@@ -1,5 +1,16 @@
+#' @title Things
+#'
+#' @description Named vector of animals and other characters e.g. Yoda, from the \code{cowsay} package
+#'
+#' @details \code{things} is a named character list of ASCII animals and characters.
+#' @export
+#' @examples
+#' things[["rms"]] %>% cat()
+#' things[["rms"]]
+#' things[["turkey"]]
+#' names(things)
 
 things <-
   append(cowsay::animals,
-    cowsay:::rms
+    list("rms" = cowsay:::rms)
   )
