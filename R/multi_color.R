cc6bfd394cefe9d39a51684be94eed1a538cb52e
aedobbyn <adobbs11@gmail.com>
2018-10-18 15:37:15 -0500

    add newhope

--- R/multi_color.R
+++ R/multi_color.R
@@ -80,7 +80,7 @@ multi_color <- function(txt = "hello world!",
     stop("All multi colors must be of class character.")
   }
 
-  direction <- match.arg(direction, c("vertical","horizontal"))
+  direction <- match.arg(direction, c("vertical", "horizontal"))
   if (is.na(direction)) stop("direction must be 'horizontal' or 'vertical'.")
 
   colors <- insert_rainbow(colors)
--- R/textshapes.R
+++ R/textshapes.R
@@ -4,25 +4,32 @@
 #' @details Use positive step argument for upward pointing triangle
 #' and negative step and wider maxlen for downward pointing triangle.
 #' @param string (character) Some text to reshape into a triangle.
-#' @param maxlen (integer) width of top of triangle, defaults to 1, set larger than 1 for downward-pointing triangle
+#' @param maxlen (integer) Width of top of triangle. Defaults to 1. Set larger than 1 for downward-pointing triangle.
 #' @param step (integer) Number of characters to expand or contract triangle width per line (set to negative for downward-pointing triangle)
 #' @param display (logical) Returns string invisibly if FALSE (default), set to TRUE to display returned string
 #'
 #' @return A string
 #'
+#' @examples
+#' triangle_string("hellooooooooooooooooooooooooooooooooooooooooooooooooooo world") %>%
+#'   multi_color()
 
-triangle_string <- function(string, maxlen=1, step=+1, display=FALSE) {
+triangle_string <- function(string, maxlen = 1, step = +1, display = FALSE) {
   # if maxlen == 0 then stop, no more lines left
-  if (maxlen == 0) return('\n')
+  if (maxlen == 0) return("\n")
 
   # string shorter than maxlen, return full string
   if (stringr::str_length(string) <= maxlen) return(string)
 
   # recursively print maxlen characters and start new line
-  output <- rev(stringr::str_c(stringr::str_sub(string, 1, maxlen),'\n',
-                      triangle_string(stringr::str_sub(string, maxlen+1, stringr::str_length(string)), maxlen=maxlen+step, step=step)))
+  output <- rev(stringr::str_c(
+    stringr::str_sub(string, 1, maxlen), "\n",
+    triangle_string(stringr::str_sub(string, maxlen + 1, stringr::str_length(string)),
+                    maxlen = maxlen + step,
+                    step = step)
+  ))
 
-  ifelse(display, return(output), return(invisible(output)) )
+  ifelse(display, return(output), return(invisible(output)))
 }
 
 
@@ -32,27 +39,33 @@ triangle_string <- function(string, maxlen=1, step=+1, display=FALSE) {
 #' @details To removes last line break set \code{removelastbreak} to TRUE.
 #'
 #' @param string (character) Some text to center within console.
-#' @param removelastbreak (logical) Set to TRUE to removes last line break, defaults to FALSE.
+#' @param remove_last_break (logical) Set to TRUE to remove last line break. Defaults to FALSE.
 #' @param display (logical) Returns string invisibly if FALSE (default), set to TRUE to display returned string
 #'
 #' @return A string
 #'
+#' @examples
+#' triangle_string(starwars_intro, display = TRUE) %>%
+#'   center_string() %>%
+#'   multi_color(direction = "horizontal", recycle_chars = TRUE)
 
-center_string <- function(string, removelastbreak=TRUE, display=FALSE) {
+center_string <- function(string, remove_last_break = TRUE, display = FALSE) {
   width <- getOption("width")
-  output <- ''
+  output <- ""
 
-  strings <- stringr::str_split(string, "\n")
-  strings <- unlist(strings)
+  strings <- stringr::str_split(string, "\n") %>% unlist()
+
   for (string in strings) {
-    spaces <- stringr::str_flatten(rep(" ", floor((width - stringr::str_length(string))/2)))
-    output <- stringr::str_c(output, spaces, string, spaces,'\n') }
-
-  if (removelastbreak==T) {
-    output <- stringi::stri_replace_last(output,fixed='\n','')
-    output <- stringi::stri_replace_last(output,fixed='\n','')
-    output <- stringi::stri_replace_last(output,regex ='\\s+','')
-    output <- stringi::stri_replace_last(output,regex ='\\s+','')
+    buffer_len <- floor((width - stringr::str_length(string)) / 2)
+    if (buffer_len < 0) buffer_len <- 0
+    spaces <- stringr::str_flatten(rep(" ", buffer_len))
+    output <- stringr::str_c(output, spaces, string, spaces, "\n")
   }
 
-  ifelse(display, return(output), return(invisible(output)))   }
+  if (remove_last_break == TRUE) {
+    output <- stringi::stri_replace_last(output, fixed = "\n", "")
+    output <- stringi::stri_replace_last(output, regex = "\\s+", "")
+  }
+
+  ifelse(display, return(output), return(invisible(output)))
+}
